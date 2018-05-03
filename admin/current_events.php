742d821c7f54fd6b89d87211788c301d6e2f6613
MarcelJurtz <jurtzmarcel@gmail.com>
2018-05-03 20:30:36 +0200

    Fix formatting

--- admin/current_events.php
+++ admin/current_events.php
@@ -24,72 +24,69 @@ $unfinishedEvents = $repo->GetAllUnfinishedEvents();
 foreach($unfinishedEvents as $event) {
     $eventParticipations = $repo->GetEventParticipations($event);
 
-   
-
     if(sizeof($eventParticipations) > 0) {
-        
+
         echo "<h3>$event->Designation</h3>";
-    
-    echo "<table>
-    <thead>
-      <tr>
-        <td>Benutzer</td>
-        <td>Zugesagt</td>
-        <td>Spieler</td>
-        <td>Coach</td>
-        <td>Umpire</td>
-        <td>Scorer</td>
-        <td>Plätze</td>
-        <td>Bemerkung</td>
-      </tr>
-    </thead>
-    <tbody>";
+        echo "<table>
+        <thead>
+        <tr>
+            <td>Benutzer</td>
+            <td>Zugesagt</td>
+            <td>Spieler</td>
+            <td>Coach</td>
+            <td>Umpire</td>
+            <td>Scorer</td>
+            <td>Plätze</td>
+            <td>Bemerkung</td>
+        </tr>
+        </thead>
+        <tbody>";
 
-    foreach($eventParticipations as $participation) {
+        foreach($eventParticipations as $participation) {
 
-        if($participation->Accepted == 0) {
-            $participation->Accepted = "Nein";
-        } else {
-            $participation->Accepted = "Ja";
+            if($participation->Accepted == 0) {
+                $participation->Accepted = "Nein";
+            } else {
+                $participation->Accepted = "Ja";
+            }
+
+            if($participation->IsPlayer == 0) {
+                $participation->IsPlayer = "Nein";
+            } else {
+                $participation->IsPlayer = "Ja";
+            }
+
+            if($participation->IsCoach == 0) {
+                $participation->IsCoach = "Nein";
+            } else {
+                $participation->IsCoach = "Ja";
+            }
+
+            if($participation->IsUmpire == 0) {
+                $participation->IsUmpire = "Nein";
+            } else {
+                $participation->IsUmpire = "Ja";
+            }
+
+            if($participation->IsScorer == 0) {
+                $participation->IsScorer = "Nein";
+            } else {
+                $participation->IsScorer = "Ja";
+            }
+
+            echo "<tr>
+                <td>$participation->Username</td>
+                <td>$participation->Accepted</td>
+                <td>$participation->IsPlayer</td>
+                <td>$participation->IsCoach</td>
+                <td>$participation->IsUmpire</td>
+                <td>$participation->IsScorer</td>
+                <td>$participation->Seats</td>
+                <td>$participation->Note</td>
+            </tr>";
         }
 
-        if($participation->IsPlayer == 0) {
-            $participation->IsPlayer = "Nein";
-        } else {
-            $participation->IsPlayer = "Ja";
-        }
-
-        if($participation->IsCoach == 0) {
-            $participation->IsCoach = "Nein";
-        } else {
-            $participation->IsCoach = "Ja";
-        }
-
-        if($participation->IsUmpire == 0) {
-            $participation->IsUmpire = "Nein";
-        } else {
-            $participation->IsUmpire = "Ja";
-        }
-
-        if($participation->IsScorer == 0) {
-            $participation->IsScorer = "Nein";
-        } else {
-            $participation->IsScorer = "Ja";
-        }
-
-        echo "<tr>
-            <td>$participation->Username</td>
-            <td>$participation->Accepted</td>
-            <td>$participation->IsPlayer</td>
-            <td>$participation->IsCoach</td>
-            <td>$participation->IsUmpire</td>
-            <td>$participation->IsScorer</td>
-            <td>$participation->Seats</td>
-            <td>$participation->Note</td>
-        </tr>";
-    }
-
-    echo "</tbody></table>";
+        echo "</tbody></table>";
     }
 }
 
